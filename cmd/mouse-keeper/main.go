ac4a7994460406fb8dc947f35c9ec1556e830ac0
Hootrix <hhtjim@gmail.com>
2024-12-30 11:04:59 +0800

    optimize

--- cmd/mouse-keeper/main.go
+++ cmd/mouse-keeper/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	VERSION      string = "0.1.6"
+	VERSION      string = "0.1.7"
 	URL          string = "https://www.hhtjim.com"
 	RuningStatus string = "..." //●
 	PauseStatus  string = "   " //○
